c0e4a03067cc51680078cd51eb2ef1377b8d0936
Ronan Brett <Ronan.Brett@theaa.ie>
2016-09-20 17:02:34 +0100

    Update Auth Guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,16 +5,18 @@ import { CountriesComponent } from './pages/countries/countries.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { FundingCategoriesComponent } from './pages/funding-categories/funding-categories.component';
 import { EntriesComponent } from './pages/entries/entries.component';
+import { CanActivateViaAuthGuard } from './guards/authGuard';
+
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'funding-categories', component: FundingCategoriesComponent },
-  { path: 'countries', component: CountriesComponent },
-  { path: 'entries', component: EntriesComponent }
+  { path: 'categories', component: CategoriesComponent, canActivate: [CanActivateViaAuthGuard] },
+  { path: 'funding-categories', component: FundingCategoriesComponent, canActivate: [CanActivateViaAuthGuard] },
+  { path: 'countries', component: CountriesComponent, canActivate: [CanActivateViaAuthGuard] },
+  { path: 'entries', component: EntriesComponent, canActivate: [CanActivateViaAuthGuard] }
 ];
 
 export const appRoutingProviders: any[] = [
-
+  CanActivateViaAuthGuard
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
